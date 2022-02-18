14a135ba11005b546bf2bb34618d79b573781f2c
schoolalexis <68104681+schoolalexis@users.noreply.github.com>
2022-02-18 21:45:16 +0100

    tests 3/3

--- src/app/components/rgpd/rgpd.component.spec.ts
+++ src/app/components/rgpd/rgpd.component.spec.ts
@@ -19,7 +19,31 @@ describe('RgpdComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  /**
+   * Sert à vérifier s'il existe un composant RGPD
+   * @return boolean:true
+   */
+  it('should have had an rgpd component', (() => {
+    const fixture = TestBed.createComponent(RgpdComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  /**
+   * Sert à vérifier le titre, ayant pour id 'title'
+   * @return string:'Mentions Légales'
+   */
+  it('should have had the title ...', () => {
+    const btn = fixture.debugElement.nativeElement.querySelector('#title');
+    expect(btn.innerHTML).toBe('Mentions Légales');
+  });
+
+  /**
+   * Sert à vérifier si la feuille de style, à b
+   */
+  it('should have had no padding', () => {
+    const fixture = TestBed.createComponent(RgpdComponent);
+    const btn = fixture.debugElement.nativeElement.querySelector('div');
+    expect(btn.style.padding).toBeFalsy;
   });
 });
